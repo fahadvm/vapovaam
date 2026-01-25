b8d8636b98d290234484adec8351f200587fa2b5
fahad vm <fahadfad444@gmail.com>
2026-01-25 17:11:05 +0530

    feat:[frontend]- Admin panel with pages for managing categories and trips

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,29 @@ import { PackageDetail } from './pages/PackageDetail';
 import { Explore } from './pages/Explore';
 import { Saved } from './pages/Saved';
 import { Profile } from './pages/Profile';
+import { AdminLayout } from './pages/admin/AdminLayout';
+import { AdminDashboard } from './pages/admin/AdminDashboard';
+import { CategoriesPage } from './pages/admin/CategoriesPage';
+import { TripsPage } from './pages/admin/TripsPage';
 
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-            <Route path="/" element={<Home />} />
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
         <Route path="/saved" element={<Saved />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/package/:id" element={<PackageDetail />} />
 
+        {/* Admin Routes */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminDashboard />} />
+          <Route path="categories" element={<CategoriesPage />} />
+          <Route path="trips" element={<TripsPage />} />
+        </Route>
       </Routes>
     </HashRouter>
   );
